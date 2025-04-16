bb857d48cb8ce69ea4d255dfcbb6bb56bb4edf06
nils.wuestefeld <nils.wuestefeld@stud.uni-goettingen.de>
2025-04-16 16:33:03 +0200

    corrected imports

--- mambular/models/trambular.py
+++ mambular/models/trambular.py
@@ -1,9 +1,9 @@
 from ..base_models.trambular import Trambular
 from ..configs.trambular_config import DefaultTrambularConfig
 from ..utils.docstring_generator import generate_docstring
-from .sklearn_base_classifier import SklearnBaseClassifier
-from .sklearn_base_lss import SklearnBaseLSS
-from .sklearn_base_regressor import SklearnBaseRegressor
+from .utils.sklearn_base_classifier import SklearnBaseClassifier
+from .utils.sklearn_base_lss import SklearnBaseLSS
+from .utils.sklearn_base_regressor import SklearnBaseRegressor
 
 
 class TrambularRegressor(SklearnBaseRegressor):
